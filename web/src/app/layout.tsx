49b7bfce40afc31df26c86e78205263595849afa
krisalan <krisalan@stud.ntnu.no>
2025-10-03 14:42:14 +0200

    Update favicon metadata and manifest

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -19,8 +19,10 @@ export const metadata: Metadata = {
   manifest: "/site.webmanifest",
   icons: {
     icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
       { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
       { url: "/favicon.ico", type: "image/x-icon" },
     ],
     shortcut: "/favicon.ico",
